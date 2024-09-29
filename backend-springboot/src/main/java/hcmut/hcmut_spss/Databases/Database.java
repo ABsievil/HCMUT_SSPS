aeb84c85b8b6199d824fcd0a7b7e8b19faf28896
ABsievil <nccuong317qb@gmail.com>
2024-09-29 21:06:55 +0700

    docs(User-entity): add attribute for User class

--- backend-springboot/src/main/java/hcmut/hcmut_spss/Databases/Database.java
+++ backend-springboot/src/main/java/hcmut/hcmut_spss/Databases/Database.java
@@ -1,38 +1,38 @@
-package hcmut.hcmut_spss.Databases;
+// package hcmut.hcmut_spss.Databases;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.crypto.password.PasswordEncoder;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 
-import hcmut.hcmut_spss.Models.Role;
-import hcmut.hcmut_spss.Models.User;
-import hcmut.hcmut_spss.Repositories.UserRepository;
+// import hcmut.hcmut_spss.Models.Role;
+// import hcmut.hcmut_spss.Models.User;
+// import hcmut.hcmut_spss.Repositories.UserRepository;
 
-@Configuration
-public class Database {
-    // Logger is used to print information to terminal
-    private static final Logger logger = LoggerFactory.getLogger(Database.class);
+// @Configuration
+// public class Database {
+//     // Logger is used to print information to terminal
+//     private static final Logger logger = LoggerFactory.getLogger(Database.class);
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
+//     @Autowired
+//     PasswordEncoder passwordEncoder;
 
-    // CommandLineRunner is used to initialize data for Database (For testing)
-    @Bean
-    CommandLineRunner initDatabase(UserRepository userRepository) {
-        return new CommandLineRunner() {
-            @Override
-            public void run(String... args) throws Exception {
-                User user1 = new User(null, null, "user1", passwordEncoder.encode("1245"), "nccuong@gmail.com", Role.USER);
-                logger.info("insert user account: " + userRepository.save(user1));
-                User admin = new User(null, null, "admin", passwordEncoder.encode("1245"), "nccuong2@gmail.com", Role.ADMIN);
-                logger.info("insert admin account: " + userRepository.save(admin));
-            }
-        };
-    }
-}
+//     // CommandLineRunner is used to initialize data for Database (For testing)
+//     @Bean
+//     CommandLineRunner initDatabase(UserRepository userRepository) {
+//         return new CommandLineRunner() {
+//             @Override
+//             public void run(String... args) throws Exception {
+//                 User user1 = new User(null, null, "user1", passwordEncoder.encode("1245"), "nccuong@gmail.com", Role.USER);
+//                 logger.info("insert user account: " + userRepository.save(user1));
+//                 User admin = new User(null, null, "admin", passwordEncoder.encode("1245"), "nccuong2@gmail.com", Role.ADMIN);
+//                 logger.info("insert admin account: " + userRepository.save(admin));
+//             }
+//         };
+//     }
+// }
 
