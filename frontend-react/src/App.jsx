d26604ce411770e8c7b269c115a941cb456a28bd
Hồ Nhật Trị <honhattri2004@gmail.com>
2024-10-01 22:53:18 +0700

    docs(page-path): create link to loginpage and registerpage

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,8 +1,9 @@
-import { Routes, Route } from 'react-router-dom';
-import HomePage from "../src/components/root/HomePage"
-import PrintingSystem from "../src/components/root/PrintPage"
-import PrintingPage from "../src/components/root/BuyPaper"
-
+import { Routes, Route } from "react-router-dom";
+import HomePage from "../src/components/root/HomePage";
+import PrintingSystem from "../src/components/root/PrintPage";
+import PrintingPage from "../src/components/root/BuyPaper";
+import LoginPage from "../src/components/root/LoginPage";
+import RegisterPage from "./components/root/Login/Register";
 export default function App() {
   return (
     <div>
@@ -10,7 +11,9 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/print" element={<PrintingSystem />} />
         <Route path="/buyPaper" element={<PrintingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/regis" element={<RegisterPage />} />
       </Routes>
     </div>
-  )
+  );
 }
