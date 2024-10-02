8da0393da4c57eb8cde9525612f4d8473faf34db
Hồ Nhật Trị <honhattri2004@gmail.com>
2024-10-02 22:36:04 +0700

    docs(fe): add direction to page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -4,6 +4,10 @@ import PrintingSystem from "../src/components/root/PrintPage";
 import PrintingPage from "../src/components/root/BuyPaper";
 import LoginPage from "../src/components/root/LoginPage";
 import RegisterPage from "../src/components/root/RegisterPage";
+import VerificationRegister from "./components/root/VerificationRegister";
+import VerificationMail from "./components/root/Email/VerifycationMail";
+import CheckCodeFromMail from "./components/root/Email/CheckCodeMail";
+import CreateNewPassword from "./components/root/Email/CreateNewPassword";
 export default function App() {
   return (
     <div>
@@ -13,6 +17,10 @@ export default function App() {
         <Route path="/buyPaper" element={<PrintingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/regis" element={<RegisterPage />} />
+        <Route path="/verify" element={<VerificationRegister />} />
+        <Route path="/verifymail" element={<VerificationMail />} />
+        <Route path="/codemail" element={<CheckCodeFromMail />} />
+        <Route path="/newpassword" element={<CreateNewPassword />} />
       </Routes>
     </div>
   );
