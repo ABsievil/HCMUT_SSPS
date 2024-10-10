9ab21473cd23b522dc9716f9113167dd16f5119e
Gloxiniaaa <hnm142758@gamil.com>
2024-10-10 21:22:39 +0700

    docs(printlog-filter): filter page type (a3, a4, a5)

--- frontend-react/src/components/root/PrintLog/PrintLogFilter.jsx
+++ frontend-react/src/components/root/PrintLog/PrintLogFilter.jsx
@@ -9,6 +9,29 @@ const PrintLogFilter = () => {
     <div className="p-4">
 
       <div className="flex justify-center items-center space-x-4">
+      <div>
+          <label className="block text-sm font-medium text-gray-700">
+            LOẠI GIẤY IN
+          </label>
+          <select
+            value={printerId}
+            onChange={(e) => setPrinterId(e.target.value)}
+            className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+          >
+            <option value="" disabled>
+              Khổ Giấy
+            </option>
+            <option value="h6-104" >
+              A4
+            </option>
+            <option value="h6-102">
+              A3
+            </option>
+            <option value="h1-210">
+              A5
+            </option>
+          </select>
+        </div>
         <div>
           <label className="block text-sm font-medium text-gray-700">
             CHỌN MÁY IN
