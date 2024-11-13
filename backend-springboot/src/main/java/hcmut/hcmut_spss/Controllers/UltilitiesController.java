207c785c91bb9bbd890e39727fb7d02c230840de
ABsievil <nccuong317qb@gmail.com>
2024-11-13 17:20:11 +0700

    docs(BE-render-data): add methods config for Utility attribute

--- backend-springboot/src/main/java/hcmut/hcmut_spss/Controllers/UltilitiesController.java
+++ backend-springboot/src/main/java/hcmut/hcmut_spss/Controllers/UltilitiesController.java
@@ -1,5 +1,7 @@
 package hcmut.hcmut_spss.Controllers;
 
+import java.sql.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import hcmut.hcmut_spss.DTO.ResponseObject;
@@ -29,4 +32,43 @@ public class UltilitiesController {
         return ultilitiesService.PROC_addUtility(utilityDTO);
     }
 
+    @PutMapping("/insertFileType")
+    public ResponseEntity<ResponseObject> insertFileType(
+                        @RequestParam("semester") String semester, 
+                        @RequestParam("typeAccepted") String typeAccepted){
+
+        return ultilitiesService.PROC_insertFileType(semester, typeAccepted);
+    }
+
+    @PutMapping("/deleteFileType")
+    public ResponseEntity<ResponseObject> deleteFileType(
+                        @RequestParam("semester") String semester, 
+                        @RequestParam("typeAccepted") String typeAccepted){
+
+        return ultilitiesService.PROC_deleteFileType(semester, typeAccepted);
+    }
+
+    @PutMapping("/updateDateReset")
+    public ResponseEntity<ResponseObject> updateDateReset(
+                        @RequestParam("semester") String semester, 
+                        @RequestParam("resetDate") Date resetDate){
+
+        return ultilitiesService.PROC_updateDateReset(semester, resetDate);
+    }
+
+    @PutMapping("/updatePagePrice")
+    public ResponseEntity<ResponseObject> updatePagePrice(
+                        @RequestParam("semester") String semester, 
+                        @RequestParam("pagePrice") Integer pagePrice){
+
+        return ultilitiesService.PROC_updatePagePrice(semester, pagePrice);
+    }
+
+    @PutMapping("/updateDefaultPage")
+    public ResponseEntity<ResponseObject> updateDefaultPage(
+                        @RequestParam("semester") String semester, 
+                        @RequestParam("defaultPage") Integer defaultPage){
+
+        return ultilitiesService.PROC_updateDefaultPage(semester, defaultPage);
+    }
 }
--- backend-springboot/src/main/java/hcmut/hcmut_spss/Services/UltilitiesService.java
+++ backend-springboot/src/main/java/hcmut/hcmut_spss/Services/UltilitiesService.java
@@ -81,5 +81,123 @@ public class UltilitiesService {
         }
     }
 
+    public ResponseEntity<ResponseObject> PROC_insertFileType(String semester, String typeAccepted){
+        try {
+            jdbcTemplate.execute(
+            "CALL insert_file_type(?, ?)",
+            (PreparedStatementCallback<Void>) ps -> {
+                ps.setString(1, semester); 
+                ps.setString(2, typeAccepted);
+                ps.execute();
+                return null;
+            }
+        );
+            return ResponseEntity.status(HttpStatus.OK)
+                .body(new ResponseObject("OK", "Query to update PROC_insertFileType() successfully", null));
+        } catch (DataAccessException e) {
+            // Xử lý lỗi liên quan đến truy cập dữ liệu
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseObject("ERROR", "Database error: " + e.getMessage(), null));
+        } catch (Exception e) {
+            // Xử lý các lỗi khác
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseObject("ERROR", "Error updating PROC_insertFileType(): " + e.getMessage(), null));
+        }
+    }
 
+    public ResponseEntity<ResponseObject> PROC_deleteFileType(String semester, String typeAccepted){
+        try {
+            jdbcTemplate.execute(
+            "CALL delete_file_type(?, ?)",
+            (PreparedStatementCallback<Void>) ps -> {
+                ps.setString(1, semester); 
+                ps.setString(2, typeAccepted);
+                ps.execute();
+                return null;
+            }
+        );
+            return ResponseEntity.status(HttpStatus.OK)
+                .body(new ResponseObject("OK", "Query to update PROC_deleteFileType() successfully", null));
+        } catch (DataAccessException e) {
+            // Xử lý lỗi liên quan đến truy cập dữ liệu
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseObject("ERROR", "Database error: " + e.getMessage(), null));
+        } catch (Exception e) {
+            // Xử lý các lỗi khác
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseObject("ERROR", "Error updating PROC_deleteFileType(): " + e.getMessage(), null));
+        }
+    }
+
+    public ResponseEntity<ResponseObject> PROC_updateDateReset(String semester, Date resetDate){
+        try {
+            jdbcTemplate.execute(
+            "CALL update_date_reset(?, ?)",
+            (PreparedStatementCallback<Void>) ps -> {
+                ps.setString(1, semester); 
+                ps.setDate(2, resetDate);
+                ps.execute();
+                return null;
+            }
+        );
+            return ResponseEntity.status(HttpStatus.OK)
+                .body(new ResponseObject("OK", "Query to update PROC_updateDateReset() successfully", null));
+        } catch (DataAccessException e) {
+            // Xử lý lỗi liên quan đến truy cập dữ liệu
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseObject("ERROR", "Database error: " + e.getMessage(), null));
+        } catch (Exception e) {
+            // Xử lý các lỗi khác
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseObject("ERROR", "Error updating PROC_updateDateReset(): " + e.getMessage(), null));
+        }
+    }
+
+    public ResponseEntity<ResponseObject> PROC_updatePagePrice(String semester, Integer pagePrice){
+        try {
+            jdbcTemplate.execute(
+            "CALL update_page_price(?, ?)",
+            (PreparedStatementCallback<Void>) ps -> {
+                ps.setString(1, semester); 
+                ps.setInt(2, pagePrice);
+                ps.execute();
+                return null;
+            }
+        );
+            return ResponseEntity.status(HttpStatus.OK)
+                .body(new ResponseObject("OK", "Query to update PROC_updatePagePrice() successfully", null));
+        } catch (DataAccessException e) {
+            // Xử lý lỗi liên quan đến truy cập dữ liệu
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseObject("ERROR", "Database error: " + e.getMessage(), null));
+        } catch (Exception e) {
+            // Xử lý các lỗi khác
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseObject("ERROR", "Error updating PROC_updatePagePrice(): " + e.getMessage(), null));
+        }
+    }
+
+    public ResponseEntity<ResponseObject> PROC_updateDefaultPage(String semester, Integer defaultPage){
+        try {
+            jdbcTemplate.execute(
+            "CALL update_default_page(?, ?)",
+            (PreparedStatementCallback<Void>) ps -> {
+                ps.setString(1, semester); 
+                ps.setInt(2, defaultPage);
+                ps.execute();
+                return null;
+            }
+        );
+            return ResponseEntity.status(HttpStatus.OK)
+                .body(new ResponseObject("OK", "Query to update PROC_updateDefaultPage() successfully", null));
+        } catch (DataAccessException e) {
+            // Xử lý lỗi liên quan đến truy cập dữ liệu
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseObject("ERROR", "Database error: " + e.getMessage(), null));
+        } catch (Exception e) {
+            // Xử lý các lỗi khác
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseObject("ERROR", "Error updating PROC_updateDefaultPage(): " + e.getMessage(), null));
+        }
+    }
 }
