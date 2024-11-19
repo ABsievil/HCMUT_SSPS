0f337f68d8592653f3435c775c2153072778035f
Lâm Tuấn Huy <lamtuanhuy1805@gmail.com>
2024-11-19 16:52:06 +0700

    docs(fe): change name manageSemester page

--- frontend-react/src/components/root/ManageSystem.jsx
+++ frontend-react/src/components/root/ManageSystem.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Layout from './fragments/layout/Layout';
 import AddPrinter from './ManageSys/AddPrinter';
-import ManageFile from './ManageSys/ManageFile';
+import ManageSemester from './ManageSys/ManageSemester';
 
 function SystemInfoPage() {
     return (
         <Layout>
             <div className="flex flex-col md:flex-row items-center md:items-start gap-10 m-5 md:m-10">
                 <AddPrinter />
-                <ManageFile />
+                <ManageSemester />
             </div>
         </Layout>
     );
