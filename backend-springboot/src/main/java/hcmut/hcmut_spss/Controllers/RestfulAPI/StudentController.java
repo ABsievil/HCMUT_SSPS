d8d2ca2bd976d9cefa571e685277fef56842584a
ABsievil <nccuong317qb@gmail.com>
2024-11-20 00:47:07 +0700

    docs(BE): change Restful protocol for student api

--- backend-springboot/src/main/java/hcmut/hcmut_spss/Controllers/RestfulAPI/StudentController.java
+++ backend-springboot/src/main/java/hcmut/hcmut_spss/Controllers/RestfulAPI/StudentController.java
@@ -8,6 +8,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,7 +30,7 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
-    @GetMapping("/getStudentIdByUsername")
+    @PostMapping("/getStudentIdByUsername")
     public ResponseEntity<ResponseObject> FNC_getStudentIdByUsername(@RequestBody Map<String, String> requestBody) {
         String username = requestBody.get("username");
         return studentService.FNC_getStudentIdByUsername(username);
