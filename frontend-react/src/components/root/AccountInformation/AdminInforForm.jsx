b6c33564bbc5ed021d129b5dfba33cf76d07c5ff
Lâm Tuấn Huy <lamtuanhuy1805@gmail.com>
2024-11-21 23:14:56 +0700

    fix(fe): error when edit infor admin

--- frontend-react/src/components/root/AccountInformation/AdminInforForm.jsx
+++ frontend-react/src/components/root/AccountInformation/AdminInforForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Pencil } from "lucide-react";
+import { Pencil, X } from "lucide-react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { fetchAdminInfor } from "../../../store/adminInforSlice";
