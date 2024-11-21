4962d59e5034e67766ba9e5bee67de1114409dab
Lâm Tuấn Huy <lamtuanhuy1805@gmail.com>
2024-11-22 02:12:10 +0700

    build(fe): group admin and student sidebar to 1 sidebar

--- frontend-react/src/components/root/fragments/layout/Layout.jsx
+++ frontend-react/src/components/root/fragments/layout/Layout.jsx
@@ -1,25 +1,19 @@
-
 import React from "react";
-import HeaderMain from "../header/HeaderMain"
-import Footer from "../footer/Footer"
-import AdminSidebar from "../slidebar/AdminSideBar";
-import { useUser } from "../../../../store/userContext";
-import StudentSidebar from "../slidebar/StudentSidebar";
+import HeaderMain from "../header/HeaderMain";
+import Footer from "../footer/Footer";
+import Sidebar from "../slidebar/Sidebar"; // Sử dụng Sidebar chung
 
 function Layout({ children }) {
-  const { username, role, userId, isLoggedIn } = useUser();
 
   return (
     <div className="flex overflow-hidden flex-col bg-white">
       <HeaderMain />
-        <div className="flex max-md:flex-col">
-          {role === "ADMIN"?(
-            <AdminSidebar />
-          ):(<StudentSidebar />)}
-          <main className="flex flex-col mx-5 w-full max-md:ml-0 max-md:w-full">
-            {children}
-          </main>
-        </div>
+      <div className="flex max-md:flex-col">
+        <Sidebar />
+        <main className="flex flex-col mx-5 w-full max-md:ml-0 max-md:w-full">
+          {children}
+        </main>
+      </div>
       <Footer />
     </div>
   );
