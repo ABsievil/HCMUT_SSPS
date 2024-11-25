e78e2165c054c775eb3dedfeca6e4f2bbf6ffe52
ABsievil <nccuong317qb@gmail.com>
2024-11-26 00:50:10 +0700

    docs(BE-cors): allow some url from outside access to BE

--- backend-springboot/src/main/java/hcmut/hcmut_spss/Securities/WebSecurityConfig.java
+++ backend-springboot/src/main/java/hcmut/hcmut_spss/Securities/WebSecurityConfig.java
@@ -119,8 +119,12 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        // configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:8090")); // Allow your frontend origin
-        configuration.setAllowedOrigins(Arrays.asList("*"));  // Allow all origin Note: only use for testing, accepted urls should be used for security.
+        configuration.setAllowedOrigins(Arrays.asList(
+    "http://localhost:5173", 
+        "http://localhost:8090", 
+        "https://hcmut-spss.netlify.app", 
+        "https://hcmut-spss-v3-0.onrender.com")); // Allow your frontend origin
+        // configuration.setAllowedOrigins(Arrays.asList("*"));  // Allow all origin Note: only use for testing, accepted urls should be used for security.
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
