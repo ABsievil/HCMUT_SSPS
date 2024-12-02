ca1ad48421d2c5371c2dba622feb926a95331900
Gloxiniaaa <hnm142758@gamil.com>
2024-12-02 10:07:28 +0700

    feat(fe): update page remain after sending print job

--- frontend-react/src/store/personalInforSlice.jsx
+++ frontend-react/src/store/personalInforSlice.jsx
@@ -37,6 +37,23 @@ const updatePersonalInfor = async (studentDTO) => {
   }
 }
 
+const updatePageRemain = async (studentId, newValue) =>{
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_REACT_APP_BE_API_URL}/api/v1/Student/updateStudentPageRemain?studentId=${studentId}&pageRemain=${newValue}`,
+      {
+        method: 'PUT',
+        credentials: 'include',
+      }
+    );
+    if (!response.ok) {
+      throw new Error(`updatePersonalInfor failed: ${response.status}`);
+    }
+  } catch (error) {
+    console.error('Error updating PersonalInfor: ', error);
+  }
+}
+
 const personalInforSlice = createSlice({
   name: 'personalInfor',
   initialState: {
@@ -52,19 +69,7 @@ const personalInforSlice = createSlice({
         return;
       }
       state.personalInfor.data.page_remain = newValue;
-      const studentDTO = {
-        student_id: state.personalInfor.data.student_id,
-        last_name: state.personalInfor.data.last_name,
-        middle_name: state.personalInfor.data.middle_name,
-        first_name:state.personalInfor.data.first_name,
-        email: state.personalInfor.data.email,
-        date_of_birth: state.personalInfor.data.date_of_birth,
-        phone_number: state.personalInfor.data.phone_number,
-        school_year: state.personalInfor.data.school_year,
-        faculty: state.personalInfor.data.faculty,
-        page_remain: newValue
-      }
-      updatePersonalInfor(studentDTO);
+      updatePageRemain(state.personalInfor.data.student_id, newValue);
     }
   },
   extraReducers: (builder) => { // Use extraReducers for handling async actions
